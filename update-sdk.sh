18be618ed9838875a14de534bc35421f932b1e1a
Jacco Flenter <github@putpixel.nl>
2013-09-18 19:06:52 +0200

    syntax error?

--- update-sdk.sh
+++ update-sdk.sh
@@ -4,4 +4,3 @@ export ANDROID_UPDATE_FILTER="`tr '\n' ',' <$WERCKER_SOURCE_DIR/filter.list`"
 echo "export ANDROID_UPDATE_FILTER=\"${ANDROID_UPDATE_FILTER\"" | sudo tee -a $profile
 
 expect -f /pipeline/build/android-update.exp "$ANDROID_UPDATE_FILTER"
-echo "export ANDROID_UPDATE_FILTER=\"${ANDROID_UPDATE_FILTER}\"" | sudo tee -a $profile
