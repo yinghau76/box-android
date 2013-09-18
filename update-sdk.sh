0dd415df483dab352ac5c4d6ff6364a696d6ad4f
Jacco Flenter <github@putpixel.nl>
2013-09-18 16:24:40 +0200

    use comma

--- update-sdk.sh
+++ update-sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Install all available sdk tools
-export ANDROID_UPDATE_FILTER="`tr '\n' ' ' <$WERCKER_SOURCE_DIR/filter.list`"
+export ANDROID_UPDATE_FILTER="`tr '\n' ',' <$WERCKER_SOURCE_DIR/filter.list`"
 expect -f /pipeline/build/android-update.exp "$ANDROID_UPDATE_FILTER"
