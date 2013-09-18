fdeb204f99f2137fc951120240730027ae354692
Jacco Flenter <github@putpixel.nl>
2013-09-18 21:40:56 +0200

    not consistent version naming by google: not 17.0.0 but 17

--- install-sdk.sh
+++ install-sdk.sh
@@ -15,7 +15,7 @@ type android || { echo "Path: $PATH"; echo 'Android not availble after installat
 export ANDROID_BUILD_TOOLS="18.0.1"
 export ANDROID_VERSION="4.3"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
-export ANDROID_BUILD_TOOLS="17.0.0"
+export ANDROID_BUILD_TOOLS="17"
 export ANDROID_VERSION="4.2.2"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
 
