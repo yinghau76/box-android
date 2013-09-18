30580dcfa0559b0594c0f8a7e25f37b6527dccb0
Jacco Flenter <github@putpixel.nl>
2013-09-18 14:38:21 +0200

    set gradle env variables...

--- install-gradle.sh
+++ install-gradle.sh
@@ -2,5 +2,9 @@ cd /tmp
 wget http://services.gradle.org/distributions/gradle-1.7-bin.zip
 unzip gradle-1.7-bin.zip
 mv gradle-1.7 $HOME
+
 export GRADLE_HOME=$HOME/gradle-1.7
 export PATH=$PATH:$GRADLE_HOME/bin
+
+echo "export GRADLE_HOME=\"${HOME}/gradle-1.7\"" | sudo tee -a $profile
+echo 'export PATH="${PATH}:${GRADLE_HOME}/bin"' | sudo tee -a $profile
