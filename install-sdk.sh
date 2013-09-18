9a9f6390589f8129c94dc4e54a28b1037085f672
Jacco Flenter <github@putpixel.nl>
2013-09-18 17:25:11 +0200

    expose more debug info

--- install-sdk.sh
+++ install-sdk.sh
@@ -5,19 +5,22 @@ wget "http://dl.google.com/android/android-sdk_r${sdk_version}-linux.tgz"
 tar xzf "android-sdk_r${sdk_version}-linux.tgz"
 rm "android-sdk_r${sdk_version}-linux.tgz"
 
+export ANDROID_BUILD_TOOLS="18.0.1"
+
 # Set environment variables
 export ANDROID_HOME=${PWD}/android-sdk-linux
 export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 
 type android || { echo "Path: $PATH"; echo 'Android not availble after installation, terminating.'; exit 1; }
 
-wget https://dl-ssl.google.com/android/repository/build-tools_r18.0.1-linux.zip
-unzip build-tools_r18.0.1-linux.zip -d $ANDROID_HOME
+wget https://dl-ssl.google.com/android/repository/build-tools_r${ANDROID_BUILD_TOOLS}-linux.zip
+unzip build-tools_r${ANDROID_BUILD_TOOLS}-linux.zip -d $ANDROID_HOME
 mkdir -p $ANDROID_HOME/build-tools/
-mv $ANDROID_HOME/android-4.3 $ANDROID_HOME/build-tools/18.0.1
+mv $ANDROID_HOME/android-4.3 $ANDROID_HOME/build-tools/${ANDROID_BUILD_TOOLS}
 
 # Write environment variables setup to $profile
 echo "export ANDROID_HOME=\"${ANDROID_HOME}\"" | sudo tee -a $profile
+echo "export ANDROID_BUILD_TOOLS=\"${ANDROID_BUILD_TOOLS}\""
 echo "export PATH=\"${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools\"" | sudo tee -a $profile
 
 # Print $profile
--- update-sdk.sh
+++ update-sdk.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Install all available sdk tools
 export ANDROID_UPDATE_FILTER="`tr '\n' ',' <$WERCKER_SOURCE_DIR/filter.list`"
+echo "export ANDROID_UPDATE_FILTER=\"${ANDROID_UPDATE_FILTER\"" | sudo tee -a $profile
+
 expect -f /pipeline/build/android-update.exp "$ANDROID_UPDATE_FILTER"
+echo "export ANDROID_UPDATE_FILTER=\"${ANDROID_UPDATE_FILTER}\"" | sudo tee -a $profile
