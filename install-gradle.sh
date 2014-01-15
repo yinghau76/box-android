4ffe604bca41b64b16547a577a2c5618d04f91ac
Aécio Benício Fernandes <aeciobf@gmail.com>
2014-01-15 11:28:01 -0200

    Update install-gradle.sh
    Change to Gradle 1.9

--- install-gradle.sh
+++ install-gradle.sh
@@ -1,6 +1,6 @@
 cd /tmp
-wget http://services.gradle.org/distributions/gradle-1.8-bin.zip
-unzip gradle-1.8-bin.zip
-mv gradle-1.8 $HOME
+wget http://services.gradle.org/distributions/gradle-1.9-bin.zip
+unzip gradle-1.9-bin.zip
+mv gradle-1.9 $HOME
 
 
