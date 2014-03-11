3ba6e505fe864ea4d9057838b46178d9c4b7c4a9
Jacco Flenter <github@putpixel.nl>
2014-03-11 19:40:25 +0100

    update gradle to 1.11

--- install-gradle.sh
+++ install-gradle.sh
@@ -1,6 +1,6 @@
 cd /tmp
-wget http://services.gradle.org/distributions/gradle-1.9-bin.zip
-unzip gradle-1.9-bin.zip
-mv gradle-1.9 $HOME
+wget http://services.gradle.org/distributions/gradle-1.11-bin.zip
+unzip gradle-1.11-bin.zip
+mv gradle-1.11 $HOME
 
 
