b2f56d1dac1e93ab26a2af91e9b7af2d6c63ae42
Patrick Tsai <yinghau76@gmail.com>
2014-12-09 21:43:35 -0800

    Install new Android SDK components

--- install-sdk.sh
+++ install-sdk.sh
@@ -14,6 +14,9 @@ export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 
 type android || { echo "Path: $PATH"; echo 'Android not availble after installation, terminating.'; exit 1; }
 
+export ANDROID_BUILD_TOOLS="21.1.1"
+export ANDROID_VERSION="5.0.1"
+source $WERCKER_SOURCE_DIR/install-build-tools.sh
 export ANDROID_BUILD_TOOLS="20"
 export ANDROID_VERSION="4.4W"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
@@ -30,7 +33,7 @@ source $WERCKER_SOURCE_DIR/install-build-tools.sh
 # export ANDROID_VERSION="4.2.2"
 # source $WERCKER_SOURCE_DIR/install-build-tools.sh
 
-export ANDROID_BUILD_TOOLS="18.0.1,19.0.3,19.1,20"
+export ANDROID_BUILD_TOOLS="18.0.1,19.0.3,19.1,20,21.1.1"
 
 
 # Write environment variables setup to $profile
