69d85a4670aa2b1cfcda45bb4a06f415fffb584a
Patrick Tsai <yinghau76@gmail.com>
2014-12-09 22:40:34 -0800

    Update Android SDK version

--- install-sdk.sh
+++ install-sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ANDROID_SDK_VERSION="23.0.2"
+export ANDROID_SDK_VERSION="24"
 
 # Install SDK
 wget "http://dl.google.com/android/android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz"
@@ -14,7 +14,7 @@ export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 
 type android || { echo "Path: $PATH"; echo 'Android not availble after installation, terminating.'; exit 1; }
 
-export ANDROID_BUILD_TOOLS="21.1.1"
+export ANDROID_BUILD_TOOLS="21"
 export ANDROID_VERSION="5.0"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
 export ANDROID_BUILD_TOOLS="20"
@@ -33,7 +33,7 @@ source $WERCKER_SOURCE_DIR/install-build-tools.sh
 # export ANDROID_VERSION="4.2.2"
 # source $WERCKER_SOURCE_DIR/install-build-tools.sh
 
-export ANDROID_BUILD_TOOLS="18.0.1,19.0.3,19.1,20,21.1.1"
+export ANDROID_BUILD_TOOLS="18.0.1,19.0.3,19.1,20,21"
 
 
 # Write environment variables setup to $profile
