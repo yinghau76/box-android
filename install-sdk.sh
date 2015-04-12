e53b1072875e4ecae816f86041458307abd630be
cafedeaqua <cafedeaqua@users.noreply.github.com>
2015-04-12 11:25:52 +0900

    Add Android SDK build tools for 22.0.1

--- install-sdk.sh
+++ install-sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ANDROID_SDK_VERSION="24.0.2"
+export ANDROID_SDK_VERSION="24.1.2"
 
 # Install SDK
 wget "http://dl.google.com/android/android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz"
@@ -14,6 +14,9 @@ export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 
 type android || { echo "Path: $PATH"; echo 'Android not availble after installation, terminating.'; exit 1; }
 
+export ANDROID_BUILD_TOOLS="22.0.1"
+export ANDROID_VERSION="5.1"
+source $WERCKER_SOURCE_DIR/install-build-tools.sh
 export ANDROID_BUILD_TOOLS="21.1.2"
 export ANDROID_VERSION="5.0.1"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
@@ -24,7 +27,7 @@ export ANDROID_BUILD_TOOLS="19.1"
 export ANDROID_VERSION="4.4.2"
 source $WERCKER_SOURCE_DIR/install-build-tools.sh
 
-export ANDROID_BUILD_TOOLS="19.0.3,19.1,20,21.1.2"
+export ANDROID_BUILD_TOOLS="19.0.3,19.1,20,21.1.2,22.0.1"
 
 
 # Write environment variables setup to $profile
